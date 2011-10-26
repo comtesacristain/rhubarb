dbf08d0a3651a5282a591522b06bd49ca8ac04bc
Michael Sexton <Michael.Sexton@ga.gov.au>
2011-10-26 16:50:48 +1100

    Old method of calculating identified resources removed, as well as casting JORC code values

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -1,8 +1,6 @@
 class ProvincesController < ApplicationController
   before_filter :define_scope, :only => [:index]
-  #before_filter :require_user
-  # GET /provinces
-  # GET /provinces.xml
+
   def index
     unless params[:format]
       @scope = @scope.paginate :page => params[:page] , :order => 'entityid ASC' unless params[:format]
@@ -19,8 +17,6 @@ class ProvincesController < ApplicationController
     end
   end
 
-  # GET /provinces/1
-  # GET /provinces/1.xml
   def show
     @province = Province.find(params[:id])
 
@@ -32,67 +28,6 @@ class ProvincesController < ApplicationController
     end
   end
 
-  # GET /provinces/new
-  # GET /provinces/new.xml
-#  def new
-#    @province = Province.new
-#
-#    respond_to do |format|
-#      format.html # new.html.erb
-#      format.xml  { render :xml => @province }
-#    end
-#  end
-#
-#  # GET /provinces/1/edit
-#  def edit
-#    @province = Province.find(params[:id])
-#  end
-#
-#  # POST /provinces
-#  # POST /provinces.xml
-#  def create
-#    @province = Province.new(params[:province])
-#
-#    respond_to do |format|
-#      if @province.save
-#        flash[:notice] = 'Province was successfully created.'
-#        format.html { redirect_to(@province) }
-#        format.xml  { render :xml => @province, :status => :created, :location => @province }
-#      else
-#        format.html { render :action => "new" }
-#        format.xml  { render :xml => @province.errors, :status => :unprocessable_entity }
-#      end
-#    end
-#  end
-#
-#  # PUT /provinces/1
-#  # PUT /provinces/1.xml
-#  def update
-#    @province = Province.find(params[:id])
-#
-#    respond_to do |format|
-#      if @province.update_attributes(params[:province])
-#        flash[:notice] = 'Province was successfully updated.'
-#        format.html { redirect_to(@province) }
-#        format.xml  { head :ok }
-#      else
-#        format.html { render :action => "edit" }
-#        format.xml  { render :xml => @province.errors, :status => :unprocessable_entity }
-#      end
-#    end
-#  end
-#
-#  # DELETE /provinces/1
-#  # DELETE /provinces/1.xml
-#  def destroy
-#    @province = Province.find(params[:id])
-#    @province.destroy
-#
-#    respond_to do |format|
-#      format.html { redirect_to(provinces_url) }
-#      format.xml  { head :ok }
-#    end
-#  end
   private
   def define_scope
 	  scope = Province
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -14,95 +14,23 @@ class Resource < ActiveRecord::Base
   default_scope :order => "recorddate desc"
 
   scope :recent, where("mgd.resources.recorddate in (select MAX(r.recorddate) from mgd.resources r where r.eno = mgd.resources.eno)")
-	scope :zeroed, where({:pvr=>0,:pbr=>0,:ppr=>0,:mrs=>0,:idr=>0,:mid=>0,:ifr=>0,:other=>0})
+	# Change to name of scope to date
+  scope :year , lambda  { |y| {:conditions => ["mgd.resources.recorddate in (select MAX(r.recorddate) from mgd.resources r where r.eno = mgd.resources.eno and r.recorddate <= ?)", y] } }
+	scope :mineral, lambda { |min| { :include=>:resource_grades, :conditions=> ["mgd.resource_grades.commodid in (:mineral)", {:mineral => min}] } }
 
-
-  #scope :nonzero, :conditions => "(pvr <> 0 or pbr <> 0 or ppr <> 0 or mrs <> 0 or idr <> 0 or mid <> 0 or ifr <> 0 or other <> 0)"
+  scope :zeroed, where({:pvr=>0,:pbr=>0,:ppr=>0,:mrs=>0,:idr=>0,:mid=>0,:ifr=>0,:other=>0})
+  
   scope :nonzero, where("(mgd.resources.pvr <> 0 or mgd.resources.pbr <> 0 or mgd.resources.ppr <> 0 or mgd.resources.mrs <> 0 or mgd.resources.idr <> 0 or mgd.resources.mid <> 0 or mgd.resources.ifr <> 0 or mgd.resources.other <> 0)")
 
+  # For coal
   scope :recoverable, :conditions => {:rec_recoverable => 'Y'}
   scope :insitu, :conditions => {:rec_recoverable => 'N'}
 
   scope :public, :conditions=> "mgd.resources.access_code = 'O' and mgd.resources.qa_status_code = 'C'"
 
-  
-
-	scope :year , lambda  { |y| {:conditions => ["mgd.resources.recorddate in (select MAX(r.recorddate) from mgd.resources r where r.eno = mgd.resources.eno and r.recorddate <= ?)", y] } }
-
-	scope :mineral, lambda { |min| { :include=>:resource_grades, :conditions=> ["mgd.resource_grades.commodid in (:mineral)", {:mineral => min}] } }
-
-#	def self.grade(commodity, year, options={})
-#	  connection.execute("ALTER SESSION set NLS_DATE_FORMAT ='DD-MON-FXYYYY'")
-#	  year_end = '31-DEC-'+year.to_s
-#	  year_start = '01-JAN-'+year.to_s
-#	  recorddate = "select MAX(recorddate) from mgd.resources mr
-#				where mr.eno = r.eno#" # and mr.resourceno=mrg.resourceno and mrg.commodid = :commodity"
-#	  recorddate += " and recorddate <= :date"
-#	  sql = "select e1.eno, e1.entityid as deposit_name, e.entityid as zone_name, r.pvr as resource_pvr,
-#				r.pbr as resource_pbr, r.ppr as resource_ppr, r.mrs as resource_mrs, r.idr as resource_idr,
-#				r.mid as resource_mid, r.ifr as resource_ifr, r.other as resource_other, r.unit_quantity,
-#				r.inclusive, r.recorddate, r.material,
-#				rg.commodid as commodity, rg.unit_grade, rg.pvr, rg.pbr, rg.ppr, rg.mrs, rg.idr, rg.mid, rg.ifr, rg.other,
-#				rg.pvr_class1, rg.pbr_class1, rg.ppr_class1, rg.mrs_class1, rg.idr_class1, rg.mid_class1, rg.ifr_class1, rg.other_class1,
-#				rg.pvr_class2, rg.pbr_class2, rg.ppr_class2, rg.mrs_class2, rg.idr_class2, rg.mid_class2, rg.ifr_class2, rg.other_class2,
-#				rg.pvr_pcnt1, rg.pbr_pcnt1, rg.ppr_pcnt1, rg.mrs_pcnt1, rg.idr_pcnt1, rg.mid_pcnt1, rg.ifr_pcnt1, rg.other_pcnt1,
-#				rg.pvr_pcnt2, rg.pbr_pcnt2, rg.ppr_pcnt2, rg.mrs_pcnt2, rg.idr_pcnt2, rg.mid_pcnt2, rg.ifr_pcnt2, rg.other_pcnt2, ct.conversionfactor
-#				from mgd.resources r, mgd.resource_grades rg, a.entities e, a.entities e1, mgd.deposits d, mgd.commodtypes ct
-#				where r.resourceno = rg.resourceno and rg.commodid in (:commodity) and e.eno=r.eno and e.parent = e1.eno
-#				and ct.commodid = rg.commodid
-#				and e1.eno=d.eno
-#				and r.recorddate in (#{recorddate})#"
-#	  sql += " and d.state = '#{options[:state]}'" if options[:state]
-#	  sql += " and e1.eno= '#{options[:eno]}'" if options[:eno]
-#    sql += " and r.rec_recoverable = 'Y'" if options[:coal] == 'recoverable'
-#    sql += " and r.rec_recoverable = 'N'" if options[:coal] == 'insitu'
-#	  Resource.find_by_sql([sql, {:commodity=>commodity,:date=>year_end}])
-#	end
-
-  def self.aliased_commodities
-	return {'coal'=>['Cbl','Cbr','Coal'],'fluorine'=>['F','Fl','Toz'],'lithium'=>['Li','Li2O'],'magnesite'=>['Mgs','MgO'],'molybdenum'=>['Mo','MoS2'],
-		'niobium'=>['Nb','Nb2O5'],'lead_zinc'=>['Pb','Zn'],'platinum_group_elements'=>['Pt','PGE','Pd','Os','Ir','Rh','Ru'],
-		'rare_earths'=>['REO','Y2O3','REE'],'tantalum'=>['Ta','Ta2O5'],'tin'=>['Sn','SnO2'],'tungsten'=>['W','WO3'],'uranium'=>['U','U3O8'],
-		'vanadium'=>['V','V2O5']}
-  end
-
-  def pvr
-    self.pvr_before_type_cast
-  end
-
-  def pbr
-    self.pbr_before_type_cast
-  end
-
-  def ppr
-    self.ppr_before_type_cast
-  end
-
-  def mrs
-    self.mrs_before_type_cast
-  end
-
-  def idr
-    self.idr_before_type_cast
-  end
-
-  def mid
-    self.mid_before_type_cast
-  end
-
-  def ifr
-    self.ifr_before_type_cast
-  end
-
-  def other
-    self.other_before_type_cast
-  end
-
-
   def identify
     return IdentifiedResourceSet.new(self)
   end
-
 
   def zero?
     return pvr==0 && pbr==0 && ppr==0 && mrs==0 && idr==0 && mid==0 && ifr==0 && other==0
