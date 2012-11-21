88e168754fe62a0c1c8c43d3e205212ce55bd179
Michael Sexton <mick.sexton@gmail.com>
2012-11-21 11:58:31 +1100

    Change of alias.

--- app/models/commodity_type.rb
+++ app/models/commodity_type.rb
@@ -14,18 +14,19 @@ class CommodityType < ActiveRecord::Base
 	  return {'base_metals'=>['Zn','Pb','Cu'],'black_coal'=>['Cbl'],'brown_coal'=>['Cbr'],'chromium'=>['Cr','Cr2O3'],'coal'=>['Cbl','Cbr','Coal'],
       'fluorine'=>['F','Fl','Toz'],'gypsum'=>['Gp'],'kaolin'=>'Kln','lithium'=>['Li','Li2O'],'manganese'=>['MnOre','Mn'],
       'magnesite'=>['Mgs','MgO'],'molybdenum'=>['Mo','MoS2'],'niobium'=>['Nb','Nb2O5'],'zinc_lead'=>['Pb','Zn'],'mineral_sands'=>['Ilm','Rt','Zrn'],
-      'nickel_cobalt'=>['Ni','Co'],'ni_co_pge_sc'=>['Ni','Co','PGE','Pt','Pd','Os','Rh','Sc','Ir'],'ni_co_cu_pt_sc'=>['Ni','Co','Cu','Pt','Sc'],'platinum_group_elements'=>['Pt','PGE','Pd','Os','Ir','Rh','Ru'],
+      'nickel_cobalt'=>['Ni','Co'],'ni_co_pge_sc'=>['Ni','Co','PGE','Pt','Pd','Os','Rh','Sc','Ir'],
+      'ni_co_cu_pt_sc'=>['Ni','Co','Cu','Pt','Sc'],'platinum_group_elements'=>['Pt','PGE','Pd','Os','Ir','Rh','Ru'],
       'rare_earths'=>['REO','Y2O3','REE','Sc'],'tantalum'=>['Ta','Ta2O5'],'tin'=>['Sn','SnO2'],'tungsten'=>['W','WO3'],
       'uranium'=>['U','U3O8'],'vanadium'=>['V','V2O5']}
 	end
 
 	def self.aimr
-		return { 'Antimony'=>'Sb', 'Bauxite'=>'Bx', 'Black coal'=>'Cbl', 'Brown coal'=>'Cbr', 'Cadmium'=>'Cd', 'Chromium'=>'chromium', 'Cobalt'=>'Co',
-      'Copper'=>'Cu', 'Diamond'=>'Dmd', 'Fluorine'=>'fluorine', 'Gold'=>'Au','Iron Ore'=>'FeOre','Lead'=>'Pb','Lithium'=>'lithium',
-      'Magnesite'=>'magnesite', 'Manganese Ore'=>'MnOre','Mineral Sands'=>'mineral_sands','Molybdenum'=>'molybdenum','Nickel'=>'Ni',
-      'Niobium'=>'niobium','Phosphate rock'=>'Phos','Platinum Group Elements'=>'platinum_group_elements','Rare Earths'=>'rare_earths',
-      'Shale oil'=>'Osh','Silver'=>'Ag','Tantalum'=>'tantalum','Thorium'=>'Th','Tin'=>'tin','Tungsten'=>'tungsten','Uranium'=>'uranium',
-      'Vanadium'=>'vanadium','Zinc'=>'Zn'}
+		return { 'Antimony'=>'Sb', 'Bauxite'=>'Bx', 'Black coal'=>'Cbl', 'Brown coal'=>'Cbr', 'Chromium'=>'Cr', 'Cobalt'=>'Co',
+      'Copper'=>'Cu', 'Diamond'=>'Dmd', 'Fluorine'=>'F', 'Gold'=>'Au','Iron Ore'=>'FeOre','Lead'=>'Pb','Lithium'=>'Li',
+      'Magnesite'=>'Mgs', 'Manganese Ore'=>'MnOre','Mineral Sands'=>'mineral_sands','Molybdenum'=>'Mo','Nickel'=>'Ni',
+      'Niobium'=>'Nb','Phosphate rock'=>'Phos','Platinum Group Elements'=>'platinum_group_elements','Rare Earths'=>'rare_earths',
+      'Shale oil'=>'Osh','Silver'=>'Ag','Tantalum'=>'Ta','Thorium'=>'Th','Tin'=>'Sn','Tungsten'=>'W','Uranium'=>'U',
+      'Vanadium'=>'V','Zinc'=>'Zn'}
 	end
 
   def self.all_commodities
