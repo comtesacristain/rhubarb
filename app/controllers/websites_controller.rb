3d4336d94add41078f7d78ce66ed6d4d8a21955f
Michael Sexton <mick.sexton@gmail.com>
2012-11-30 16:05:57 +1100

    Added lookups for websites that have deposits - called companies for reasons of simplicity

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -9,5 +9,17 @@ class WebsitesController < ApplicationController
   def show
     @website = Website.find(params[:id])
   end
+  
+  def companies
+    unless params[:q].blank?
+      @companies=Website.by_name(params[:q]).find_with_deposit_as_keys
+    else
+      @companies=Website.find_with_deposit_as_keys
+    end
+    
+     respond_to do |format|
+       format.json { render :json =>@companies}
+     end
+  end
 
 end
