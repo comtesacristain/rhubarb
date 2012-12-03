bd9dc7ee54bcf6226b4579d449b995c4516a9f94
Michael Sexton <mick.sexton@gmail.com>
2012-12-03 13:58:22 +1100

    Updates for tokeninput.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function () {
+  $('#vessel').tokenInput('/surveys/vessels.json', { 
+  	crossDomain: false,
+  	tokenLimit: 1,
+  	resultsLimit: 20,
+  	prePopulate: $('#vessel').data('pre'),
+  	});
+  	
+  	 $('#name').tokenInput('/deposits/names.json', { 
+  	crossDomain: false,
+  	tokenLimit: 1,
+  	resultsLimit: 20,
+  	prePopulate: $('#name').data('pre'),
+  //	minChars: 3
+  	});
+});
