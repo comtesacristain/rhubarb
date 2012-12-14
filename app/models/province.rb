0fdd4c6925a429aa61187396f867d110857a2f84
Michael Sexton <mick.sexton@gmail.com>
2012-12-14 13:45:30 +1100

    Lookups added

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,7 @@
 class Province < Entity
-  default_scope where(:entity_type => 'PROVINCE')#, :order => "entityid"
+  def self.default_scope
+    where(:entity_type => 'PROVINCE').merge(super)
+  end
 
   has_one :province_status, :class_name => "ProvinceStatus", :foreign_key => :eno
 
@@ -25,5 +27,24 @@ class Province < Entity
   has_many :province_deposits, :class_name => "ProvinceDeposit", :foreign_key => :eno
   has_many :deposits, :through => :province_deposits
 
+  # Commodities
+  has_many :commodities, :through => :deposits
+
   scope :by_name, lambda { |name| { :conditions=> ["UPPER(a.entities.entityid) like UPPER(:name)",{:name=> "%#{name}%"}] } }
+  
+  
+  # Key Lookups
+  def self.find_with_deposit_as_keys(id=nil)
+    #TODO where instead of find is dangerous when number of things is over 1000
+    unless id.nil?
+      self.where(:eno=>id).collect{|p| {:id=>p.eno,:name=>p.entityid}}
+    else
+      self.where(:eno=>ProvinceDeposit.uniq.pluck(:eno)).collect{|p| {:id=>p.eno,:name=>p.entityid}}
+    end
+    
+  end 
+  
+  
+  
+  
 end
