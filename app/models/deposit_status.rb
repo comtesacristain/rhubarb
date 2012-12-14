4644c77302de487c79ae160640f69c2e83bb6936
Michael Sexton <mick.sexton@gmail.com>
2012-12-14 13:23:15 +1100

    Added lookups

--- app/models/deposit_status.rb
+++ app/models/deposit_status.rb
@@ -1,26 +1,62 @@
 class DepositStatus < ActiveRecord::Base
+   #TODO set relation with State
+  # TODO Ugly name for the model. Think of a better one. Maybe worth calling _this_ the deposit model and the other one 'coordinates'
   connection.execute("ALTER SESSION set NLS_DATE_FORMAT ='DD-MON-FXYYYY'")
 
   set_table_name "mgd.deposits"
   set_primary_key :eno
-
+  set_date_columns :entrydate, :qadate, :lastupdate
+  
+  
   #belongs_to :zone, :class_name => "Zone",  :foreign_key => :parent
 
   belongs_to :deposit, :class_name => "Deposit", :foreign_key => :eno
-
+  
+  # Queries 
+  #TODO Change lambda scopes to functions
   scope :state, lambda { |s| where(:state=>s) }
   scope :status, lambda { |s| where(:operating_status=>s) }
+  
+  
+  
   def self.statuses
     return ActiveSupport::OrderedHash["All",nil,"Operating Mines","operating mine","Historic Mines","historic mine","Mineral Deposits","mineral deposit"]
   end
-
+  
+  #TODO Change this to use key lookups, find all atlas statuses in project first and change to atlas_status_as_keys 
+    
   def self.atlas_statuses
     return ActiveSupport::OrderedHash["Operating Mines","operating mine","Historic Mines","historic mine","Mineral Deposits","mineral deposit"]
   end
 
-  def self.states
-    {}
+  def self.operating_statuses
+    return self.uniq.pluck(:operating_status)
   end
 
-  set_date_columns :entrydate, :qadate, :lastupdate
+  def self.states
+    return self.uniq.pluck(:state)
+  end
+  
+  # Key Lookups
+  
+  def self.operating_statuses_as_keys(id=nil)
+    unless id.nil?
+      self.where(:operating_status=>id).operating_statuses.collect{|os| {:id=>os,:name=>os.titleize}}
+    else
+      self.operating_statuses.collect{|os| {:id=>os,:name=>os.titleize}}
+    end
+  end
+  
+  def self.states_as_keys(id=nil)
+    return State.where(:stateid=>DepositStatus.states).find_as_keys
+    
+    # TODO Probably needs something for querying
+    #unless id.nil?
+    #  return State.find_as_keys(id)
+    #else
+    #  return State.find_as_keys(DepositStatus.states)
+    #end
+  end
+
+
 end
