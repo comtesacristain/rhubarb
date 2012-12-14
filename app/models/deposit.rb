9a9ea79d2ce29c120a28b01b08799f774cc5197a
Michael Sexton <mick.sexton@gmail.com>
2012-12-14 13:25:05 +1100

    Added province tokens and lookups for forms.

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,6 +1,8 @@
 class Deposit < Entity
-  
-  default_scope where(:entity_type => 'MINERAL DEPOSIT')#, :order => "entityid"
+  attr_reader :province_tokens  
+  def self.default_scope
+    where(:entity_type => 'MINERAL DEPOSIT').merge(super)
+  end
 
   has_one :deposit_status, :class_name => "DepositStatus", :foreign_key => :eno
 
@@ -23,13 +25,34 @@ class Deposit < Entity
   has_many :bloblinks, :class_name => "Bloblink", :foreign_key => :source_no
 
   # Provinces
-  has_many :province_deposits, :class_name => "ProvinceDeposit", :foreign_key => :deposno
+  has_many :province_deposits, :foreign_key => :deposno
+  
+  
+  #accepts_nested_attributes_for :regrocks
+  
   has_many :provinces, :through => :province_deposits
+  
+  
+  def province_tokens=(ids)
+    self.province_ids=ids.split(",")
+  end
 
   scope :mineral, lambda { |min| { :include=>:commodities, :conditions=> ["mgd.commods.commodid in (?)", min] } }
-	scope :state, lambda { |s| { :include=>:deposit_status, :conditions=> ["mgd.deposits.state = ?", s] } }
-	scope :status, lambda { |os| { :include=>:deposit_status, :conditions=> ["mgd.deposits.operating_status = ?", os] } }
-  scope :by_name, lambda { |name| { :include=>:deposit_status, :conditions=> ["UPPER(a.entities.entityid) like UPPER(:name) or UPPER(mgd.deposits.synonyms) like UPPER(:name)",{:name=> "%#{name}%"}] } }
+	
+	#scope :state, lambda { |s| { :include=>:deposit_status, :conditions=> ["mgd.deposits.state = ?", s] } }
+	
+	def self.state(state)
+	  self.includes(:deposit_status).merge(DepositStatus.state(state))
+	end
+	
+	
+	def self.status(status)
+    self.includes(:deposit_status).merge(DepositStatus.status(status))
+  end
+  
+	
+	#scope :status, lambda { |os| { :include=>:deposit_status, :conditions=> ["mgd.deposits.operating_status = ?", os] } }
+  #scope :by_name, lambda { |name| { :include=>:deposit_status, :conditions=> ["UPPER(a.entities.entityid) like UPPER(:name) or UPPER(mgd.deposits.synonyms) like UPPER(:name)",{:name=> "%#{name}%"}] } }
 
   scope :major, :include=>:commodities, :conditions=> "commorder < 10"
   scope :minor, :include=>:commodities, :conditions=> "commorder >= 9"
@@ -38,7 +61,9 @@ class Deposit < Entity
 
   self.per_page = 10
 
-
+  
+  
+  
   def regname
     return deposit_attributes.regname.first.try(:valuename)
   end
@@ -90,4 +115,24 @@ class Deposit < Entity
   def to_param
     "#{eno}-#{entityid.parameterize}"
   end 
+  
+  
+  def self.by_name(name)
+    return self.joins(:deposit_status).where("upper (entityid) like upper('%#{name}%') or upper(synonyms) like upper('%#{name}%')")
+  end
+  
+  #listings
+  
+  #TODO Put this in Entity model
+       
+  def self.names(name=nil)
+    unless name.nil?
+      return self.by_name(name).pluck(:entityid)
+    else
+      return self.pluck(:entityid)
+    end
+  end
+
+  
+  
 end
