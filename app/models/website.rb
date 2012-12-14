efae241e3f2e94f3d9c931719436404941eccf50
Michael Sexton <mick.sexton@gmail.com>
2012-12-14 13:49:18 +1100

    Added lookups

--- app/models/website.rb
+++ app/models/website.rb
@@ -10,4 +10,19 @@ class Website < ActiveRecord::Base
   has_many :deposits, :through => :weblinks
 
 	set_date_columns :entrydate, :qadate, :lastupdate
+	
+	
+	def self.by_name(name)
+	  self.where("upper(description) like ?","%#{name.upcase}%")
+	end
+	
+	# Key Lookups
+	def self.find_with_deposit_as_keys(id=nil)
+    #TODO where instead of find is dangerous when number of things is over 1000
+    unless id.nil?
+      self.where(:websiteno=>id).collect{|w| {:id=>w.websiteno,:name=>w.description}}
+    else
+      self.where(:websiteno=>Weblink.uniq.pluck(:websiteno)).collect{|w| {:id=>w.websiteno,:name=>w.description}}
+    end
+  end 
 end
