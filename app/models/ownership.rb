70b451d2c15cc274d444898d6364a17dfbfc21f8
Michael Sexton <michael@salvatore.local>
2014-07-22 12:14:25 +1000

    Added relationship to deposits and companies

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,7 @@
 class Ownership < ActiveRecord::Base
   connection.execute("ALTER SESSION set NLS_DATE_FORMAT ='DD-MON-FXYYYY'")
   self.table_name = "mgd.ownership"
+  
+	belongs_to :deposit, :class_name => "Deposit", :foreign_key => :eno
+	belongs_to :company, :class_name => "Company", :foreign_key => :companyid
 end
