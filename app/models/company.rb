4e26d49ba3ff2555e7d489ab1d7db3574bd5d09f
Michael Sexton <michael@salvatore.local>
2014-07-22 15:26:31 +1000

    Added Comapnies key lookup

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,13 @@ class Company < ActiveRecord::Base
 
   has_many :deposits, :through => :ownerships
   
+  
+	def self.find_with_deposit_as_keys(id=nil)
+    #TODO where instead of find is dangerous when number of things is over 1000
+    unless id.nil?
+      self.where(:companyid=>id).collect{|w| {:id=>w.companyid,:name=>w.company_name}}
+    else
+      self.where(:companyid=>Ownership.uniq.pluck(:companyid)).collect{|w| {:id=>w.companyid,:name=>w.company_name}}
+    end
+  end 
 end
