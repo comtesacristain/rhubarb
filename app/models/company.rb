1882f6fefb7043be68bdc72bbe4584797f352fe3
Michael Sexton <michael@salvatore.local>
2014-07-22 15:40:15 +1000

    Added by_name query function to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,6 +7,10 @@ class Company < ActiveRecord::Base
   has_many :deposits, :through => :ownerships
   
   
+	def self.by_name(name)
+	  self.where("upper(company_name) like ?","%#{name.upcase}%")
+	end
+  
 	def self.find_with_deposit_as_keys(id=nil)
     #TODO where instead of find is dangerous when number of things is over 1000
     unless id.nil?
