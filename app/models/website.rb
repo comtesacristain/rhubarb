cfd20608a87daa9e98926657a5ba4e7a65e10897
Michael Sexton <michael@salvatore.local>
2014-07-22 15:52:00 +1000

    Shortened Website model to provide only basic web link data

--- app/models/website.rb
+++ app/models/website.rb
@@ -11,18 +11,6 @@ class Website < ActiveRecord::Base
 
 	set_date_columns :entrydate, :qadate, :lastupdate
 	
+
 	
-	def self.by_name(name)
-	  self.where("upper(description) like ?","%#{name.upcase}%")
-	end
-	
-	# Key Lookups
-	def self.find_with_deposit_as_keys(id=nil)
-    #TODO where instead of find is dangerous when number of things is over 1000
-    unless id.nil?
-      self.where(:websiteno=>id).collect{|w| {:id=>w.websiteno,:name=>w.description}}
-    else
-      self.where(:websiteno=>Weblink.uniq.pluck(:websiteno)).collect{|w| {:id=>w.websiteno,:name=>w.description}}
-    end
-  end 
 end
