def36153a5d8331e0c20c0c696a3e9e557f27a80
Michael Sexton <mick.sexton@gmail.com>
2015-08-25 15:32:21 +1000

    Removed sti_name

--- app/models/mineral_project.rb
+++ app/models/mineral_project.rb
@@ -1,9 +1,6 @@
 class MineralProject < Entity 
-  self.sti_name = "MINERAL PROJECT"
     # attr_accessible :title, :body
-  def self.default_scope
-    where(:entity_type => 'MINERAL PROJECT')
-  end
+
   
   has_one :mineral_project_status, :class_name => "DepositStatus", :foreign_key => :eno
   
