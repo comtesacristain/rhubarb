474409bd3b151f0c2ef7e737d34ce6b405311451
Michael Sexton <mick.sexton@gmail.com>
2015-08-27 15:30:28 +1000

    Only do LDAP if can find YAML file

--- config/initializers/ldap_init.rb
+++ config/initializers/ldap_init.rb
@@ -1,14 +1,17 @@
-ldap_root   = YAML.load_file("#{Rails.root}/config/ldap_root_user.yml")
-user        = ldap_root["username"]
-pass        = ldap_root["password"]
-LDAP_CONFIG = YAML.load_file("#{Rails.root}/config/ldap.yml")
-LDAP_CONNECTION = Net::LDAP.new
-LDAP_CONNECTION.host = LDAP_CONFIG["host"]
-LDAP_CONNECTION.port = LDAP_CONFIG["port"]
-LDAP_CONNECTION.base = LDAP_CONFIG["base"]
-LDAP_CONNECTION.auth "#{user}@#{LDAP_CONFIG["full_base_dn"]}", pass
-unless LDAP_CONNECTION.bind 
-  msg = LDAP_CONNECTION.get_operation_result.message
-  code = LDAP_CONNECTION.get_operation_result.code
-  raise "LDAP Connection failed: Message #{msg}, Code: #{code}"
+begin
+  ldap_root   = YAML.load_file("#{Rails.root}/config/ldap_root_user.yml")
+  user        = ldap_root["username"]
+  pass        = ldap_root["password"]
+  LDAP_CONFIG = YAML.load_file("#{Rails.root}/config/ldap.yml")
+  LDAP_CONNECTION = Net::LDAP.new
+  LDAP_CONNECTION.host = LDAP_CONFIG["host"]
+  LDAP_CONNECTION.port = LDAP_CONFIG["port"]
+  LDAP_CONNECTION.base = LDAP_CONFIG["base"]
+  LDAP_CONNECTION.auth "#{user}@#{LDAP_CONFIG["full_base_dn"]}", pass
+  unless LDAP_CONNECTION.bind 
+    msg = LDAP_CONNECTION.get_operation_result.message
+    code = LDAP_CONNECTION.get_operation_result.code
+    raise "LDAP Connection failed: Message #{msg}, Code: #{code}"
+  end
+rescue
 end
