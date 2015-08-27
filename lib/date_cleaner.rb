ecfde4be621edbc01027b5026e828bbbc1b0ec10
Michael Sexton <mick.sexton@gmail.com>
2015-08-27 23:03:18 +1000

    Date cleaner added

---
+++ lib/date_cleaner.rb
@@ -0,0 +1,15 @@
+# Paul Kay's data
+resources = Resource.where(enteredby:"U32129",qa_status_code:"U")
+resources.each do |resource|
+  if resource.recorddate >= resource.entrydate
+    comments = resource.remark
+    date_regex =  /[0-9]{1,2}[a-zA-Z]{3}[0-9]{2,4}/
+    date = date_regex.match(comments)[0]
+    if date.nil? 
+      puts "For resource #{resource.resourceno}, the following comment has no date in it:\n"
+      puts comments
+    else
+      puts "For resource #{resource.resourceno}, the following comment has a date in it:\n"
+      puts comments
+  end
+end
