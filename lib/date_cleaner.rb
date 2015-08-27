6dd03ec0378f66ccc998f0924339ceb10a323723
Michael Sexton <mick.sexton@gmail.com>
2015-08-27 23:15:10 +1000

    OK, try this

--- lib/date_cleaner.rb
+++ lib/date_cleaner.rb
@@ -3,14 +3,15 @@ resources = Resource.where(enteredby:"U32129",qa_status_code:"U")
 resources.each do |resource|
   if resource.recorddate >= resource.entrydate
     comments = resource.remark
-    date_regex =  /[0-9]{1,2}-?([0-9]|[a-zA-Z])[a-zA-Z]{2}-?[0-9]{2,4}/
+    date_regex =  /[0-9]{1,2}-?[a-zA-Z]{3}-?[0-9]{2,4}/
     dates = date_regex.match(comments)
     if dates.nil? 
       puts "For resource #{resource.resourceno}, the following comment has no date in it:\n"
       puts comments
     else
-     # puts "For resource #{resource.resourceno}, the following comment has a date in it:\n"
-     # puts comments
+      date=dates[0]
+      resource.recorddate = date.to_date
+      resource.save
     end
   end
 end
