a8cdd86d2cc35e373207e76256554caf9f6a2333
Michael Sexton <mick.sexton@gmail.com>
2015-08-27 23:11:57 +1000

    Match one or nil hyphens

--- lib/date_cleaner.rb
+++ lib/date_cleaner.rb
@@ -3,7 +3,7 @@ resources = Resource.where(enteredby:"U32129",qa_status_code:"U")
 resources.each do |resource|
   if resource.recorddate >= resource.entrydate
     comments = resource.remark
-    date_regex =  /[0-9]{1,2}([0-9]|[a-zA-Z])[a-zA-Z]{2}[0-9]{2,4}/
+    date_regex =  /[0-9]{1,2}-?([0-9]|[a-zA-Z])[a-zA-Z]{2}-?[0-9]{2,4}/
     dates = date_regex.match(comments)
     if dates.nil? 
       puts "For resource #{resource.resourceno}, the following comment has no date in it:\n"
