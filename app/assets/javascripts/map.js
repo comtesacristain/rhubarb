2215063f1729af4fb45b02e6ba1b4cdbe6c5d758
Michael Sexton <mick.sexton@gmail.com>
2015-09-08 16:55:11 +1000

    Switch off occurrences

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -8,7 +8,7 @@ var select;
 var defaultLayers = {
     //"powerstations": ["renewable" , "fossil fuel" ],
     //"major_projects" : [null],
-    "occurrences": [null],
+    //"occurrences": [null],
     "deposits" : ["historic mine" ,"mineral deposit" ,"operating mine"]
 }
 function initialize() {
