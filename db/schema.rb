e687517053a7befb76fa5c5c3c84dd22de948f6b
Michael Sexton <mick.sexton@gmail.com>
2015-09-08 17:11:21 +1000

    Not really that much different

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,96 +9,106 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100908070810) do
+ActiveRecord::Schema.define(version: 20140722015213) do
 
-  create_table "commodities", :force => true do |t|
+  create_table "commodities", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "commodity_lists", :force => true do |t|
+  create_table "commodity_lists", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "commodity_types", :force => true do |t|
+  create_table "commodity_types", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "deposits", :force => true do |t|
+  create_table "companies", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "entities", :force => true do |t|
+  create_table "entities", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "major_projects", :force => true do |t|
+  create_table "major_projects", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "occurrences", :force => true do |t|
+  create_table "mineral_projects", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "powerstations", :force => true do |t|
+  create_table "occurrences", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "provinces", :force => true do |t|
+  create_table "ownerships", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "references", :force => true do |t|
+  create_table "powerstations", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "resource_grades", :force => true do |t|
+  create_table "provinces", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "resource_references", :force => true do |t|
+  create_table "references", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "resources", :force => true do |t|
+  create_table "resource_grades", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "statuses", :force => true do |t|
+  create_table "resource_references", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "user_sessions", :force => true do |t|
+  create_table "resources", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "statuses", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_sessions", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "role",                :default => "viewer", :null => false
+    t.string   "role",                               default: "viewer", null: false
     t.string   "crypted_password"
     t.string   "password_salt"
-    t.string   "persistence_token",                         :null => false
-    t.string   "single_access_token",                       :null => false
-    t.string   "perishable_token",                          :null => false
-    t.integer  "login_count",         :default => 0,        :null => false
-    t.integer  "failed_login_count",  :default => 0,        :null => false
+    t.string   "persistence_token",                                     null: false
+    t.string   "single_access_token",                                   null: false
+    t.string   "perishable_token",                                      null: false
+    t.integer  "login_count",         precision: 38, default: 0,        null: false
+    t.integer  "failed_login_count",  precision: 38, default: 0,        null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
@@ -107,17 +118,17 @@ ActiveRecord::Schema.define(:version => 20100908070810) do
     t.datetime "updated_at"
   end
 
-  create_table "weblinks", :force => true do |t|
+  create_table "weblinks", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "websites", :force => true do |t|
+  create_table "websites", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "zones", :force => true do |t|
+  create_table "zones", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
